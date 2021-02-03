ceabeaa9af2462642789fdcaf624ca31d7441f73
Guillaume Attane <guillaume.attane@gmail.com>
2021-02-03 21:40:05 +0100

    Modification JS et ajout Jquery

--- index.js
+++ index.js
@@ -6,117 +6,154 @@ let holdBtn = document.querySelector('#holdBtn');
 
 // Recuperation des élément de jeu
 
-let scorePlayer1 = document.querySelector('scorePlayer1');
-let scorePlayer2 = document.querySelector('scorePlayer2');
-let score1 = document.querySelector('score1');
-let score2 = document.querySelector('score2');
+let scorePlayer1 = document.querySelector('#scorePlayer1');
+let scorePlayer2 = document.querySelector('#scorePlayer2');
+let score1 = document.querySelector('#score1');
+let score2 = document.querySelector('#score2');
 
-//Recuperation des donnée pour evenement JEUX
-
-let bgPlayer1 = document.querySelector('bgPlayer1');
-let bgPlayer2 = document.querySelector('bgPlayer2');
-let puce1 = document.querySelector('puce1');
-let puce2 = document.querySelector('puce2');
-let name1 = document.querySelector('#name1');
-let name2 = document.querySelector('#name2');
-
-
-// récuperation des elements constituant le dé.
-
-let d2_4_5_6 = document.querySelector('#d2_4_5_6');
-let d3_4_5_6 = document.querySelector("#d3_4_5_6");
-let d1_3_5 = document.querySelector('#d1_3_5');
-let d6 = document.querySelector('#d6');
 
 class Player {
   constructor(round, global, status) {
     this.round = round;
     this.global = global;
     this.status = status;
+
   }
 }
-var player1 = new Player(0, 0, true);
-var player2 = new Player(0, 0, false);
+let player1 = new Player(0, 0, 1);
+let player2 = new Player(0, 0, 0);
 
-init();
 
-let tempValue = 0;
+// init();
 
-newGame.addEventListener('click', (e) => {
-  init();
+newGame.addEventListener('click', () => {
+    init();
 });
 
 
 
+rollDiceBtn.addEventListener('click', () => {
 
+  if (player1.status == 1) {
+    rollDice(player1);
+    score1.textContent = player1.round;
+    console.log(player1.status);
+  } else if(player2.status == 1){
+    rollDice(player2);
+    score2.textContent = player2.round;
+  }
+});
+
+
+
+holdBtn.addEventListener('click', () =>{
+  if (player1.status == 1) {
+    hold(player1);
+    scorePlayer1.textContent = player1.global;
+    swichPlayer();
+
+  } else if (player2.status == 1) {
+    hold(player2);
+    scorePlayer2.textContent = player2.global;
+    swichPlayer();
+  }
+});
 
 
   
 function init() {
+  let name1 = document.querySelector('#name1');
+  let name2 = document.querySelector('#name2');
   name1.textContent = prompt("Entrer le nom du player 1: ");
   name2.textContent = prompt("Entrer le nom du player 2: ");
+  player1.status = 1;
+  player2.status = 0;
+  score1.textContent = 0;
+  score2.textContent = 0;
+  scorePlayer1.textContent = 0;
+  scorePlayer2.textContent = 0;
+  
 }
+
+
 
 function rollDice(player) {
   
-  tempValue = 0;
-  // displayDe(tempValue);
+  let tempValue = 0;
+  displayDe(0);
+  
   do  {
     tempValue = Math.floor(Math.random() * 6);
   }while (tempValue === 0) 
   
-  // displayDe(tempValue);
+  displayDe(tempValue);
 
   if (tempValue === 1) {
     player.round = 0;
     player.global += 0;
-    player.status = false;
-
+    swichPlayer();    
   } else {
     player.round += tempValue;
-    player.status = true;
   }
 }
 
 function hold(player) {
   
-  if (player.status === 1)
+  if (player.status)
   {
     player.global += player.round;
-    player.status = false;
+    player.round = 0;
   }
+  displayDe(0);
 }
 
-// function displayDe(value) {
 
-// switch (value) {
-  
-//   case 0:
+function swichPlayer (){
 
-//     break;
-//   case 1:
+  if (player1.status == 1) {
+    player1.status = 0;
     
-//     break;
-//   case 2:
+  } else if (player2.status == 1) {
+    player2.status = 0;
+   
+  }
 
-//     break;
-//   case 3:
+}
 
-//     break;
-//   case 4:
 
-//     break;
-//   case 5:
+function displayDe(value) {
 
-//     break;
-//   case 6:
+switch (value) {
+  
+  case 0:
+    
+    break;
+  case 1:
+    
+    break;
+  case 2:
+    
+    break;
+  case 3:
+    
+    break;
+  case 4:
+    
+    break;
+  case 5:
+    
+    break;
+  case 6:
+    d2_4_5_6.style.opacity = 1;
+    d3_4_5_6.style.opacity = 1;
+    d1_3_5.style.opacity = 0;
+    d6.style.opacity = 1;
+    break;
 
-//     break;
+  default:
+    break;
+}
 
-//   default:
-//     break;
-// }
-
-// }
+}
 
 
